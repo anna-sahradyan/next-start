a05c5c83517f7c0bddf1f6b42928c89a12783a67
Anna Paruryan <a.sahradyan72@gmail.com>
2024-07-19 18:51:45 +0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return(
+        <>
+            <h1>welcome!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam doloribus enim exercitationem facere illum impedit in iste, necessitatibus nihil nulla omnis praesentium quaerat repellendus saepe, sequi soluta ullam? Atque, officiis!</p>
+        </>
+    )
+}
